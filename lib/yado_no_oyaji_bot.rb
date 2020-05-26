2ae1cdfb7f52b07cc5bc3e4fb171191a725d2fc5
popotanu <scrapmacaron@gmail.com>
2020-05-27 04:29:10 +0900

    generate oyaji

--- lib/yado_no_oyaji_bot.rb
+++ lib/yado_no_oyaji_bot.rb
@@ -14,15 +14,16 @@ require 'faraday'
 
 module YadoNoOyajiBot
 
-require_relative 'yado_no_oyaji_bot/config'
+require_relative 'yado_no_oyaji_bot/setting'
+SETTINGS = Settings.new
 
-settings = Settings.new
-
+Dir[File.dirname(__FILE__), 'yado_no_oyaji_bot', '*rb'].join(',').each { |f| require f }
 
   bot = Discordrb::Commands::CommandBot.new(
-    #initialize
-
+    token: SETTINGS.token,
+    client_id: SETTINGS.client_id
   )
 end
+
 
 
