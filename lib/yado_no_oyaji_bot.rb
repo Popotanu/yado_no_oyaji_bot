195cb04b0780d00d13fdc3fccc0a4223aa69cd32
popotanu <scrapmacaron@gmail.com>
2020-05-26 10:08:23 +0900

    initialize oyaji

--- lib/yado_no_oyaji_bot.rb
+++ lib/yado_no_oyaji_bot.rb
@@ -3,3 +3,19 @@
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
+
+require 'uri'
+require 'yaml'
+require 'open-uri'
+require 'faraday'
+
+require_relative 'yado_no_oyaji_bot/config'
+
+module YadoNoOyajiBot
+  bot = Discordrb::Commands::CommandBot.new(
+    #initialize
+
+  )
+end
+
+
